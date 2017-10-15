2df42e9a824ee46c670e60103be7db114a681547
Frank Brückner <dev@froschdesignstudio.de>
2017-10-15 21:43:08 +0200

    Adds unit test for NavigationHelperFactory

---
+++ test/Helper/Service/NavigationHelperFactoryTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-view-navigation for the canonical source repository
+ * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   https://github.com/zendframework/zend-view-navigation/blob/master/LICENSE.md New BSD License
+ */
+
+namespace Helper\Service;
+
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\ServiceManager;
+use Zend\View\Navigation\Helper\Navigation as NavigationHelper;
+use Zend\View\Navigation\Helper\Service\NavigationHelperFactory;
+use PHPUnit\Framework\TestCase;
+
+class NavigationHelperFactoryTest extends TestCase
+{
+    /**
+     * @var NavigationHelperFactory
+     */
+    private $factory;
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    protected function setUp()
+    {
+        $this->factory   = new NavigationHelperFactory();
+        $this->container = new ServiceManager();
+    }
+
+    public function testFactoryShouldReturnNavigationHelper()
+    {
+        $helper = ($this->factory)($this->container, 'test');
+
+        $this->assertInstanceOf(NavigationHelper::class, $helper);
+    }
+
+    public function testFactoryShouldInjectContainer()
+    {
+        /** @var NavigationHelper $helper */
+        $helper = ($this->factory)($this->container, 'test');
+
+        $this->assertSame($this->container, $helper->getNavigationManager());
+    }
+}
